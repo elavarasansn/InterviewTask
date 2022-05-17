77b433cdeead223368844f21971f18ea336f1258
Elavarasan S <elavarasansn@gmail.com>
2022-05-17 14:56:08 +0530

    Update strCalculater.js

--- strCalculater.js
+++ strCalculater.js
@@ -6,12 +6,25 @@ let sum=0;
 		
 		
     str_number = str_number.replace(/[//]/g,"").replace(/\n/g,",").replace(/^;/, '').replace(";",",")
-    sum  = str_number.match(/\d+/g).map(Number).reduce((p,c)=>{ return parseInt(p)+parseInt(c);})
+    
+	let num_arr  = str_number.match(/-?\d+/g).map(Number);
+	
+	
+		
+	let negative = num_arr.filter(function (a) { return a < 0; })
+	
+	
+	
+	
+	if(negative.length > 0)
+	   throw new Error(`negatives not allowed ${negative.toString()}`)
+	
+	sum = num_arr.reduce((p,c)=>{ return parseInt(p)+parseInt(c);})
     
     
     return sum;
     
-}  
+}
 add("");
 add("1");
 add("1\n2,3");
