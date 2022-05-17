82d6777334cbd396713518938336da6697371608
Elavarasan S <elavarasansn@gmail.com>
2022-05-17 11:54:50 +0530

    Update strCalculater.js

--- strCalculater.js
+++ strCalculater.js
@@ -2,4 +2,11 @@ function add(str_number){
 let sum=0;
     if(str_number=="")
         return 0;
+    
+    sum  = str_number.split("").reduce((p,c)=>{ return parseInt(p)+parseInt(c);})
+    console.log(str_number.split(""))
+    console.log(sum)
+    return sum;
+    
 } 
+add("25");
